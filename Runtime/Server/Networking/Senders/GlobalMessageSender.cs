92e33b3edcd9fee89074ed1904d94ff5e163eefd
HeaX <heax@moonsama.com>
2023-11-11 16:59:26 +0100

    feat: inherit scoped message sender instead of implementing it

--- Runtime/Server/Networking/Senders/GlobalMessageSender.cs
+++ Runtime/Server/Networking/Senders/GlobalMessageSender.cs
@@ -4,16 +4,16 @@ using MultiplayerProtocol.Senders;
 
 namespace WebsocketMultiplayer.Server.Senders
 {
-    public abstract class GlobalMessageSender : IScopedMessageSender
+    public abstract class GlobalMessageSender : ScopedMessageSender
     {
         protected IMultiplayerServer server { get; }
-        
+
         protected GlobalMessageSender(IMultiplayerServer server)
         {
             this.server = server;
         }
 
-        public IEnumerable<NetworkConnection> GetConnections()
+        public override IEnumerable<NetworkConnection> GetConnections()
         {
             return server.store.connection.GetConnections();
         }
